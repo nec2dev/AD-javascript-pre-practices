c8ad32ab5b739e59c6509bbcff325e673a752021
nec2dev <nec2multivackergithub@outlook.com>
2021-03-16 17:28:12 -0300

    code

--- 1-Introducción/code.js
+++ 1-Introducción/code.js
@@ -1,1 +1,14 @@
-alert('Hello')
+/*
+Introducción ¿Qué es javascript?
+   -es un lenguaje de programación
+   -interpretado
+   -Orientado a objetos
+   -imperativo
+   -case sensitive
+   -basado en prototipos7instancias7clases
+   -tipado débil
+   -lenguaje dinámico
+*/
+
+/*Edito de texto*/
+alert('Hello');
---
+++ 1-Introducción/variables.js
@@ -0,0 +1,42 @@
+/*
+Variables
+ -Concepto
+ -Ejemplo
+ -Tipos de datos: String, number, boolean
+ -Casos especiales de datos
+    -Undefined
+    -Null
+    -Nan
+ -Scope
+ -Hoisting
+ -Tipos de variables
+     -Let
+     -Var
+     -Const
+ -Crear múltiples variables
+ -Pruebas con prompt*/
+
+ //Variable: Espacio de memoria asignado en un programa
+variable = "Nahuel";
+alert(variable);
+
+/*Tipos de Datos: Strings: Son cadenas de caracteres que se escriben entre
+                  comillas simples o dobles*/
+string = 'Cadena de texto';
+
+/*Tipos de Datos: Number: Son números y pueden ser enteros, decimales, se
+                  escriben sin ningún tipo de*/
+number = 40;
+
+/*Tipos de Datos: Booleanos: Arrojan solo dos resultados, verdadero o falso,
+                  true or false, 0 ó 1*/
+boolean = true;   
+
+/*Casos especiales de tipos de datos: Las variables se pueden declarar, inicializar
+y modificar a lo largo de la ejecución del programa*/
+//inicialización e una variable
+var cadena = 'Hello';
+let numero = 29;
+const nombre = 'Nahuel'
+nombre = 'Manuel'
+
