a3fd14849e6b5ab314f7178c068cc3f0964999cd
nec2dev <nec2multivackergithub@outlook.com>
2021-03-17 15:54:14 -0300

    concat

--- 1-Introducción/concatenacion.js
+++ 1-Introducción/concatenacion.js
@@ -5,4 +5,26 @@
 -con Concat
 -con backtiks y con la variable entre ${}*/
 
+saludo = 'Hola Mayra, Buen día!';
+pregunta = ' Cómo estás?';
+
+frase = saludo + pregunta;
+
+document.write(frase);
+
+num1 = 5;
+num2 = 8;
+num3 = '12'
+//Tipado dinámico, forzamos el string con las comillas
+suma = '' + num1 + num2;
+//Concat, método de js, necesita que 1 solo elemnto sea string
+concat = num3.concat(num2);
+
+document.write(suma);
+document.write(concat);
+
+frase1 = 'Nahuel Correa'
+
+
+
 
