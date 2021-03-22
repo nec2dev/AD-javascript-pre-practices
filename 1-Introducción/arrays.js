414b4f43f5d0ea633a9365f9af9f2937e4afeab6
nec2dev <nec2multivackergithub@outlook.com>
2021-03-22 15:29:42 -0300

    arrays sin métodos

--- 1-Introducción/arrays.js
+++ 1-Introducción/arrays.js
@@ -1,7 +1,19 @@
 /*
+Una estructura típica en todos los lenguajes es el Array, 
+que es como una variable donde podemos introducir varios valores, 
+en lugar de solamente uno como ocurre con la variables normales.
+
+Los arrays nos permiten guardar varias variables y acceder a ellas 
+de manera independiente, es como tener una variable con distintos 
+compartimentos donde podemos introducir datos distintos. 
+Para ello utilizamos un índice que nos permite especificar el 
+compartimiento o posición a la que nos estamos refiriendo.
 */
 let arrays = ["Nahuel", 40, "Comuna 9"];
-let frameworks = ["Angular","ReactJS","EmberJS","VueJS"];
+let frameworks = ["Angular","ReactJS","EmberJS","VueJS", 2,3.4,5,100];
 
-document.write(frameworks);
+// document.write(frameworks);
+document.write(frameworks[1]);
+document.write("   ");
+document.write(frameworks[5]);
 
