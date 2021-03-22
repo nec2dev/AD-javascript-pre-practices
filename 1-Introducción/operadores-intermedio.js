ce93e36030b6d6e6914a1fe39615a5a97241b9bb
nec2dev <nec2multivackergithub@outlook.com>
2021-03-22 13:59:35 -0300

    tablas de verdad

--- 1-Introducción/operadores-intermedio.js
+++ 1-Introducción/operadores-intermedio.js
@@ -32,5 +32,46 @@ document.write(num1 < num2);
 /*Los operadores lógicos nos devuelve un resultado a partir 
   de que se cumpla (o no) una condición, su resultado es 
     booleano. y sus operandos son valores lógicos o
-    asimilables a ellos*/
+    asimilables a ellos. Tablas de verdad*/
+
+let valor1 = true;
+let valor2 = true;
+
+let resultado1 = valor1 && valor2;
+let resultado2 = valor1 || valor2;
+let resultado3 = !valor1;
+
+document.write(resultado1);
+
+num1 = 12;
+num2 = 24;
+
+afirmacion1 = num1 < num2;
+afirmacion2 = num1 != num2;
+
+document.write(afirmacion1 && afirmacion2);
+document.write(afirmacion1 || afirmacion2);
+document.write(num1 < num2 || num1 == num2);
+
+// document.write(true && true);
+// document.write(false && true);
+// document.write(true && false);
+// document.write(false && false);
+
+// document.write(true || true);
+// document.write(false || true);
+// document.write(true || false);
+// document.write(false || false);
+
+// document.write(!false);
+// document.write(!true);
+
+num3 = 25;
+num4 = 92;
+num5 = 91;
+
+op = (num1 < num2 || num2 < num3) && (!num1 && num5 != num3);
+
+document.write(op);
+
     
