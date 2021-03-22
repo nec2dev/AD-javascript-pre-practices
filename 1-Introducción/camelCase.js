0a3f7323038ed925cc9f6f8deb5af08f78f7fb89
nec2dev <nec2multivackergithub@outlook.com>
2021-03-22 14:09:47 -0300

    agregué script de camelcase

---
+++ 1-Introducción/camelCase.js
@@ -0,0 +1,17 @@
+/*
+es un estilo de escritura que se aplica a frases o palabras compuestas. 
+El nombre se debe a que las mayúsculas a lo largo de una palabra en 
+CamelCase se asemejan a las jorobas de un camello. 
+El nombre CamelCase se podría traducir como Mayúsculas/Minúsculas Camello.
+El término case se traduce como "caja tipográfica", que a su vez implica 
+si una letra es mayúscula o minúscula y tiene su origen en la disposición 
+os tipos móviles en casilleros o cajas.
+Existen dos tipos de CamelCase:
+
+UpperCamelCase (más conocido como PascalCase), cuando la primera letra de 
+cada una de las palabras es mayúscula. 
+    Ejemplo: EjemploDeUpperCamelCase.
+lowerCamelCase (o simplemente CamelCase), igual que la anterior con la 
+excepción de que la primera letra es minúscula. 
+    Ejemplo: ejemploDeLowerCamelCase.
+*/
