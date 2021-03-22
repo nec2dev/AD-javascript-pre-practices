8e6798631258518751110e374dd63e71da77c1b7
nec2dev <nec2multivackergithub@outlook.com>
2021-03-22 14:02:21 -0300

    operadores lógicos

--- 1-Introducción/operadores-intermedio.js
+++ 1-Introducción/operadores-intermedio.js
@@ -70,7 +70,7 @@ num3 = 25;
 num4 = 92;
 num5 = 91;
 
-op = (num1 < num2 || num2 < num3) && (!num1 && num5 != num3);
+op = (num1 < num2 || num2 < num3) && (!(num1 != num2) && num5 != num3);
 
 document.write(op);
 
