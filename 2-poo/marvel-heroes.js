64cf498ef83a6ee2c8c501aea55ad7160d025e51
nec2dev <nec2multivackergithub@outlook.com>
2021-03-23 18:59:17 -0300

    arrancamos con poo

---
+++ 2-poo/marvel-heroes.js
@@ -0,0 +1,41 @@
+class marvelheroes {
+    constructor(nombreheroe, nombrereal, identidad, altura, peso, cabello, universo){
+        this.nombreheroe = nombreheroe;
+        this.nombrereal = nombrereal;
+        this.identidad = identidad;
+        this.altura = altura;
+        this.peso = peso;
+        this.cabello = cabello;
+        this.universo = universo;
+        this.dataclasificada = `Alias actual: ${this.nombreheroe}<br>
+                                Nombre real: ${this.nombrereal}<br>
+                                Identidad: ${this.identidad}<br>
+                                Altura: ${this.altura}<br>
+                                Peso: ${this.peso}<br>
+                                Cabello: ${this.cabello}<br>
+                                Universo: ${this.universo}<br><br>`
+    }
+    verdataclasificada(){
+        document.write(this.dataclasificada);
+    }
+}
+
+
+let heroe001 = new marvelheroes("Scarlet Witch", "Wanda Maximoff", "Pública", 1.7, 60, "Castaño", "Tierra-616");
+let heroe002 = new marvelheroes("Vision", "Vision", "Pública", 1.85, 136, "Calvo", "Tierra-616");
+let heroe003 = new marvelheroes("Charles Francis Xavier", "Profesor X", "Pública", 1.83, 86, "Calvo", "Tierra-616");
+let heroe004 = new marvelheroes("Jane Foster", "Valkyrie", "Secreta", 1.75, 204, "Rubio", "Tierra-616");
+let heroe005 = new marvelheroes("Groot", "Groot", "Pública", null, 3719, "null", "Tierra-616");
+
+// document.write(heroe001.identidad);
+// document.write(heroe001.dataclasificada);
+// document.write(heroe002.dataclasificada);
+// document.write(heroe003.dataclasificada);
+// document.write(heroe004.dataclasificada);
+// document.write(heroe005.dataclasificada);
+
+heroe001.verdataclasificada;
+heroe002.verdataclasificada;
+heroe003.verdataclasificada;
+heroe004.verdataclasificada;
+heroe005.verdataclasificada;
