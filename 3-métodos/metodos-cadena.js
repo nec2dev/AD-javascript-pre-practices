53cbf4e17026074c9213e861e1fd867088db60db
nec2dev <nec2multivackergithub@outlook.com>
2021-03-23 20:31:33 -0300

    includes

--- 3-métodos/metodos-cadena.js
+++ 3-métodos/metodos-cadena.js
@@ -1,8 +1,30 @@
 // concat(): Junta dos o más cadenas de texto y devuelve 1
 let cadena = "La acadena de caracteres";
 let cadena1 = " Se concatena con esta nueva cadena de caracteres"
+let cadena2 = "La acadena ";
+let cadena3 = "de caracteres";
 // res = cadena.concat(" Se concatena con esta nueva cadena de caracteres");
 res = cadena.concat(cadena1);
-document.write(res);
+document.write(res + "<br>");
+document.write("<br>");
+
+/*startsWith(): Si una cadena termina con los caracteres de otra
+cadena, devuelve true, de lo contrario, devuelve false*/
+res = cadena.startsWith(cadena1);
+document.write(res + "<br>");
+res = cadena.startsWith(cadena2)
+document.write(res + "<br>");
+document.write("<br>");
+
+/*endsWith(): Si una cadena termina con los caracteres de otra
+cadena, devuelve true, de lo contrario, devuelve false*/
+res = cadena.endsWith(cadena3);
+document.write(res + "<br>");
+
+/*includes(): Si una cadena se encuentra dentro de otra cadena,
+devuelve true, de lo contrario, devuelve false*/
+res = cadena.includes(cadena3);
+document.write(res + "<br>");
+
 
 
