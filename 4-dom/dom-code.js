0ee1bf3c2fbca0ef9a4498df55cf55e482a0aa7e
Nahuel <71109200+nec2dev@users.noreply.github.com>
2021-03-25 19:46:58 -0300

    getElementsByTagName

--- 4-dom/dom-code.js
+++ 4-dom/dom-code.js
@@ -2,4 +2,7 @@ let parrafo = "Buenas, soy un párrafo <br>";
 document.write(parrafo + "<br>");
 
 parrafo = document.getElementById("parrafo");
-document.write(parrafo);
+document.write(parrafo) + "<br>");
+
+parrafo = document.getElementsByTagName("p");
+document.write(parrafo + "<br>");
