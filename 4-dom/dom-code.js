e47223b30f84671262317effae74a310de49e562
Nahuel <71109200+nec2dev@users.noreply.github.com>
2021-03-25 19:55:37 -0300

    querySelectorAll

--- 4-dom/dom-code.js
+++ 4-dom/dom-code.js
@@ -2,7 +2,10 @@ let parrafo = "Buenas, soy un párrafo <br>";
 document.write(parrafo + "<br>");
 
 parrafo = document.getElementById("parrafo");
-document.write(parrafo) + "<br>");
+document.write(parrafo + "<br>");
 
 parrafo = document.getElementsByTagName("p");
+document.write(parrafo + "<br>");
+
+parrafo = document.querySelectorAll("p");
 document.write(parrafo + "<br>");
