58f3759f9ce1bb791f5fb4b22d469ea518a23cf5
Nahuel <71109200+nec2dev@users.noreply.github.com>
2021-03-29 21:43:03 -0300

    more window

--- 5-objectwindow/objectwindow.js
+++ 5-objectwindow/objectwindow.js
@@ -1,7 +1,56 @@
 /*Window Object
+El objeto window representa la ventana que contiene un 
+documento DOM; la propiedad document apunta al DOM do-
+cument cargado en esa ventana. El objeto window al que 
+pertenece un documento puede ser obtenido usando la 
+propiedad document.defaultView.
+
 open(): Carga un recurso en el contexto de un nuevo 
 navegador (como una ventana) o uno que ya existe, 
 dependiendo de los parametros.*/
 let mozillaDeveloperURL = "https://developer.mozilla.org/es/";
 let ventana = window.open(mozillaDeveloperURL);
+
+/*close(): Cierra la ventana actual, o la ventana en 
+la cual fue llamada.*/ 
 ventana.close();
+
+/*closed(): Esta propiedad de solo lectura indica si la ventana r
+eferenciada está cerrada o no.*/ 
+ventana.closed();
+
+/**/
+ventana.stop();
+
+/*alert() muestra un diálogo de alerta con 
+contenido opcional especificado y un botón OK 
+(Aceptar).*/
+alert();
+
+/*print(): Abre el diálogo para imprimir el documento 
+actual. En la mayoría de navegadores, este método bloquea 
+mientras el díalogo de impresión esté abierto, sin 
+embargo, en algunas versiones recientes de Safari podría 
+retornar de inmediato.*/
+ventana.print();
+
+/*Window.prompt() muestra un diálogo con mensaje opcional, 
+que solicita al usuario que introduzca un texto.*/
+var sign = prompt("What's your sign?");
+
+if (sign.toLowerCase() == "scorpio") {
+  alert("Wow! I'm a Scorpio too!");
+}
+
+// there are many ways to use the prompt feature
+var sign = window.prompt(); // open the blank prompt window
+var sign = prompt();       //  open the blank prompt window
+var sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
+var sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with Text "Are you feeling lucky" and default value "sure"
+
+
+ventana.print();
+
+
+confirm();
+
