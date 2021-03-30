bb8e2beefab62e7ea802c2978ce7763a0b602732
Nahuel <71109200+nec2dev@users.noreply.github.com>
2021-03-29 21:24:43 -0300

    close()

--- 5-objectwindow/objectwindow.js
+++ 5-objectwindow/objectwindow.js
@@ -2,4 +2,6 @@
 open(): Carga un recurso en el contexto de un nuevo 
 navegador (como una ventana) o uno que ya existe, 
 dependiendo de los parametros.*/
-winsow.open("https://developer.mozilla.org/es/");
+let mozillaDeveloperURL = "https://developer.mozilla.org/es/";
+let ventana = window.open(mozillaDeveloperURL);
+ventana.close();
