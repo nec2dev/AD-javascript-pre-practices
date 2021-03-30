607f69c5622ea0cb0fe265a17338f6c02406d450
Nahuel <71109200+nec2dev@users.noreply.github.com>
2021-03-29 21:20:50 -0300

    open()

---
+++ 5-objectwindow/objectwindow.js
@@ -0,0 +1,5 @@
+/*Window Object
+open(): Carga un recurso en el contexto de un nuevo 
+navegador (como una ventana) o uno que ya existe, 
+dependiendo de los parametros.*/
+winsow.open("https://developer.mozilla.org/es/");
